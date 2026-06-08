f893f36d32622bd69ad3a5986cb6d0b5dd97ba68
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-08 18:04:00 +0300

    Initial Point

---
+++ src/main/java/com/sustainabilitytracker/SustainabilityTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.sustainabilitytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SustainabilityTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SustainabilityTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sustainabilitytracker/SustainabilityTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sustainabilitytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustainabilityTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
