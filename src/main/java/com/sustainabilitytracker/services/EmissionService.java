19219c2f3ac9f4c1cc2e3194da5339cd37a55629
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-10 23:10:59 +0300

    feat: add emission service.

---
+++ src/main/java/com/sustainabilitytracker/services/EmissionService.java
@@ -0,0 +1,13 @@
+package com.sustainabilitytracker.services;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@AllArgsConstructor
+public class EmissionService {
+
+
+}
