387ed01a19ace4afb245cf949c7c8f3994df920d
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-11 00:42:00 +0300

    feat: add controller with submit energy endpoint.

---
+++ src/main/java/com/sustainabilitytracker/controllers/EnergyController.java
@@ -0,0 +1,43 @@
+package com.sustainabilitytracker.controllers;
+
+import com.sustainabilitytracker.dtos.requests.EnergyRequest;
+import com.sustainabilitytracker.dtos.requests.RejectRequest;
+import com.sustainabilitytracker.dtos.responses.EnergyResponse;
+import com.sustainabilitytracker.dtos.responses.EnergySummaryResponse;
+import com.sustainabilitytracker.services.EnergyService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.List;
+
+@AllArgsConstructor
+@Controller
+@RequestMapping("/energies")
+public class EnergyController {
+
+    private final EnergyService energyService;
+
+    // SUBMIT ENERGY
+    @PostMapping
+    public ResponseEntity<EnergyResponse> submitEnergy(
+            @Valid @RequestBody EnergyRequest energyRequest,
+            UriComponentsBuilder uriBuilder) {
+
+        EnergyResponse energyResponse = energyService.submitEnergy(energyRequest);
+
+        var uri = uriBuilder.path("/energy/{energyId}")
+                .buildAndExpand(energyResponse.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(energyResponse);
+    }
+
+
+}
