ba303ccf2e1698f8941d12d788042ced6a4b1c57
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-24 22:39:28 +0300

    refactor: add more annotation

--- src/main/java/com/sustainabilitytracker/dtos/responses/EnergySummaryResponse.java
+++ src/main/java/com/sustainabilitytracker/dtos/responses/EnergySummaryResponse.java
@@ -1,16 +1,21 @@
 package com.sustainabilitytracker.dtos.responses;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EnergySummaryResponse {
     private BigDecimal totalKwh;
     private BigDecimal totalRenewableKwh;
     private BigDecimal averageKwh;
+    private BigDecimal renewablePercentage;
     private String period;
     private Integer recordCount;
 }
