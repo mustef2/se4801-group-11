ab3b02bd7f16696a1ed1bdae5a2755ea79fead7f
owner <swedhan734@gmail.com>
2026-06-26 19:08:27 +0300

    feat(repositories): add WaterRepository

--- src/main/java/com/sustainabilitytracker/repositories/WaterRepository.java
+++ src/main/java/com/sustainabilitytracker/repositories/WaterRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
