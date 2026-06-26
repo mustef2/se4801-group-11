12abcf508747ef4beb3cab36950d6789449a237d
owner <swedhan734@gmail.com>
2026-06-26 18:42:42 +0300

    feat(repositories): add AuditRepository

---
+++ src/main/java/com/sustainabilitytracker/repositories/AuditRepository.java
@@ -0,0 +1,22 @@
+package com.sustainabilitytracker.repositories;
+
+import com.sustainabilitytracker.sustainabilitytracker.entities.AuditRecord;
+import com.sustainabilitytracker.sustainabilitytracker.enums.AuditAction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AuditRepository extends JpaRepository<AuditRecord, Long> {
+
+    List<AuditRecord> findByReportIdOrderByCreatedAtDesc(Long reportId);
+
+    List<AuditRecord> findByAuditorId(Long auditorId);
+
+    List<AuditRecord> findByCompanyId(Long companyId);
+
+    int countByReportIdAndAction(Long reportId, AuditAction action);
+
+    boolean existsByReportIdAndAuditorId(Long reportId, Long auditorId);
+}
