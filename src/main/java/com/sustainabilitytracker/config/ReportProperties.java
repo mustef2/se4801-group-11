dae0e4544de0453b4770e7514202667d29de2cb9
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-26 22:32:37 +0300

    feat(service): add and update services

---
+++ src/main/java/com/sustainabilitytracker/config/ReportProperties.java
@@ -0,0 +1,13 @@
+package com.sustainabilitytracker.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "app.report")
+public class ReportProperties {
+
+    private String storagePath;
+}
