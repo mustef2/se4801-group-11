ffd02ab298dd51f09811aa42544c074e4dc32010
owner <swedhan734@gmail.com>
2026-06-26 18:50:58 +0300

    feat(repositories): add CompanyRepository

--- src/main/java/com/sustainabilitytracker/repositories/CompanyRepository.java
+++ src/main/java/com/sustainabilitytracker/repositories/CompanyRepository.java
@@ -1,6 +1,6 @@
 package com.sustainabilitytracker.repositories;
 
-import com.sustainabilitytracker.sustainabilitytracker.entities.Company;
+import com.sustainabilitytracker.entities.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
