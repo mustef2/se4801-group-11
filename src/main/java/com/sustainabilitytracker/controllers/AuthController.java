b13328e1ade4f4a5b1ba0c233e074bcea834109e
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-28 14:14:33 +0300

    prepare CI/CD config with Github action and add cors config

--- src/main/java/com/sustainabilitytracker/controllers/AuthController.java
+++ src/main/java/com/sustainabilitytracker/controllers/AuthController.java
@@ -5,6 +5,7 @@ import com.sustainabilitytracker.dtos.UserDto;
 import com.sustainabilitytracker.dtos.requests.ChangePasswordRequest;
 import com.sustainabilitytracker.dtos.requests.LoginRequest;
 import com.sustainabilitytracker.dtos.responses.JwtResponse;
+import com.sustainabilitytracker.dtos.responses.LoginResponse;
 import com.sustainabilitytracker.mapper.UserMapper;
 import com.sustainabilitytracker.repositories.UserRepository;
 import com.sustainabilitytracker.security.JwtTokenProvider;
@@ -25,65 +26,29 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
     private final JwtTokenProvider jwtTokenProvider;
     private final JwtProperties jwtProperties;
     private final AuthService authService;
-    private final UserMapper userMapper;
 
     @PostMapping("/login")
-    public ResponseEntity<JwtResponse> login(
+    public ResponseEntity<LoginResponse> login(
             @RequestBody LoginRequest request,
-            HttpServletResponse response){
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.getEmail(),
-                        request.getPassword()
-                )
-        );
-        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
-
-        var accessToken = jwtTokenProvider.generateAccessToken(user);
-        var refreshToken = jwtTokenProvider.generateRefreshToken(user);
-
-        var cookie = new Cookie("refreshToken", refreshToken);
-        cookie.setHttpOnly(true);
-        cookie.setPath("/auth/refresh");
-        cookie.setMaxAge(jwtProperties.getRefreshTokenExpiration());
-        cookie.setSecure(true);
-        response.addCookie(cookie);
-
-        return ResponseEntity.ok(new JwtResponse(accessToken));
+            HttpServletResponse response) {
+        return ResponseEntity.ok(authService.login(request, response));
     }
 
     @PostMapping("/change-password")
-    public ResponseEntity<Void> changePassword(@Valid @RequestBody ChangePasswordRequest request ) {
+    public ResponseEntity<Void> changePassword(@Valid @RequestBody ChangePasswordRequest request) {
         authService.changePassword(request);
         return ResponseEntity.ok().build();
     }
 
-
     @PostMapping("/refresh")
-    public ResponseEntity<JwtResponse> refresh(@CookieValue(value = "refreshToken") String refreshToken){
-//        var jwt = jwtService.parseToken(refreshToken);
-        if (refreshToken == null || jwtTokenProvider.isTokenValid(refreshToken)) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-
-        var user = userRepository.findById(Long.valueOf(jwtTokenProvider.extractUserIdFromToken(refreshToken))).orElseThrow();
-        var accessToken = jwtTokenProvider.generateAccessToken(user);
-
-        return ResponseEntity.ok(new JwtResponse(accessToken));
+    public ResponseEntity<LoginResponse> refresh(
+            @CookieValue(value = "refreshToken", required = false) String refreshToken,
+            HttpServletResponse response) {
+        return ResponseEntity.ok(authService.refresh(refreshToken, response));
     }
 
-    @GetMapping("/me")
-    public ResponseEntity<UserDto> me(){
-        var authentication = SecurityContextHolder.getContext().getAuthentication();
-        var userId =(Long) authentication.getPrincipal();
-
-        var user = userRepository.findById(userId).orElse(null);
-        if (user == null) return ResponseEntity.notFound().build();
-        var userDto = userMapper.toDto(user);
-        return ResponseEntity.ok(userDto);
-    }
 }
-
